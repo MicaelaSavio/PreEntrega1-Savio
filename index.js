17007d13211933b2bdd85f59d07a77d7f3477d46
MicaelaSavio <micaela.savio@hotmail.com>
2023-04-26 17:54:38 -0300

    modifique el index

--- index.js
+++ index.js
@@ -1,22 +1,76 @@
-// const invitados = {
-//     invitado1: "marcos",
-//     invitado2: "julieta",
-//     invitado3: "maria",
-//     invitado4: "roxana",
-//     invitado5: "mabel",
-// }
-// ;
+// En principio se le pide al usuario que ingrese el celular con el cual hizo su registro en la pagina para acceder a la calculadora.
 
-// console.log (invitados.invitado1);
-
-// invitados.invitado1 = "julian";
-
-// console.log (invitados.invitado1);
+let usuario = prompt ("Porfavor si esta registrado ingrese su celular para ingresar")
+while (usuario != "123456"){
+    alert ("usuario incorrecto")
+    let usuario = prompt ("Ingrese su celular para ingresar") 
+   
+}
+alert ("Bienvenida");
 
 
-const nombres = ["maria", "juan", "rosalia", "claudia"];
+// Luego se le solicita al usuario su nombre para darle la bienvenida. 
 
 
-for(let index = 0; index < nombres.length; index++){
-    console.log (nombres[index]);
+alert ("Cual es tu nombre?");
+let nombre = prompt ("Ingresa tu nombre");
+let resultado =  `${nombre}`;
+let saludo = "Hola ";
+let final = " Bienvenido a la pagina";
+
+alert (saludo + nombre + final);
+
+// Aca estaria la calculadora que realiza las 4 operaciones: suma, resta, multiplicacion y division. 
+
+const sumar = (num1,num2)=>{
+    return parseInt(num1) + parseInt (num2);
+}
+const restar = (num1,num2)=>{
+    return parseInt(num1) - parseInt (num2);
+}
+const multiplicar = (num1,num2)=>{
+    return parseInt(num1) * parseInt (num2);
+}
+const dividir = (num1,num2)=>{
+    return parseInt(num1) / parseInt (num2);
+}
+
+alert ("¿Que operacion quisieras realizar?");
+let operacion = prompt ("1: sumar, 2: restar, 3: multiplicar, 4: dividir");
+
+if (operacion == 1, sumar) {
+    let numero1 = prompt ("primer numero para sumar");
+    let numero2 = prompt ("segundo numero para sumar");
+    resultado = sumar (numero1,numero2);
+    alert (`tu resultado es ${resultado}`);
+}
+else if (operacion == 2, restar) {
+    let numero1 = prompt ("primer numero para restar");
+    let numero2 = prompt ("segundo numero para restar");
+    resultado = restar (numero1,numero2);
+    alert (`tu resultado es ${resultado}`);
+}
+
+else if (operacion == 3, multiplicar) {
+    let numero1 = prompt ("primer numero para multiplicar");
+    let numero2 = prompt ("segundo numero para multiplicar");
+    resultado = multiplicar (numero1,numero2);
+    alert (`tu resultado es ${resultado}`);
+}
+
+else if (operacion == 4, dividir) {
+    let numero1 = prompt ("primer numero para dividir");
+    let numero2 = prompt ("segundo numero para dividir");
+    resultado = dividir (numero1,numero2);
+    alert (`tu resultado es ${resultado}`);
+}
+
+// Por ultimo se le consulta al usuario si fue favorable el uso de la calculadora.
+
+let inicio = prompt ("Te gustaron las operaciones matematicas, responde: si / no ?")
+
+if (inicio.toLowerCase () === "si") {
+    alert("Que bueno leer eso! pronto tendremos mas actividades para ti :)");
+} else {
+    alert("Lamentamos que no haya sido de tu agrado.");
 }
