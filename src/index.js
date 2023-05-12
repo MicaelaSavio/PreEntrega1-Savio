dab4a306a53b4b099165862b71ce37841ba1b6e1
MicaelaSavio <micaela.savio@hotmail.com>
2023-05-12 15:07:19 -0300

    cambios realizados

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function suma (num1, num2) {
+    let resultado = num1 + num2;
+    return resultado
+}
+
+let resultado = suma (40,40)
+document.write (resultado);
+
+function saludar (nombre){
+    let frase = `Hola ${nombre} bienvenido`;
+    document.write (frase)
+}
+
+
+
+
+saludar ("mi amor");
+
+let cadena = "micaela ";
+let cadena2 = "";
+
+resultado = cadena.repeat (18);
+document.write (resultado);
